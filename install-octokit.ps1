ea630d62bca512f6f1f6e354f0c533605ee28a20
Theomegas1 <prophesies@hotmail.com>
2025-08-24 21:51:48 -0400

    Add install-octokit.ps1 script for automating Octokit.NET installation

---
+++ install-octokit.ps1
@@ -0,0 +1,4 @@
+# PowerShell Script to Install Octokit.NET in the Current .NET Project
+Write-Host "Installing Octokit.NET via dotnet CLI..."
+dotnet add package Octokit
+Write-Host "Octokit.NET package installation complete."
